8f2ce100e569b7fdc35cf7da927c44e8d4ee5708
Milla Rwana de araújo Silva <mras3@cin.ufpe.br>
2025-11-18 10:52:46 -0300

    Update exception.c
    Mudanças-parte 2

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -1,62 +1,47 @@
 #include "userprog/exception.h"
 #include <inttypes.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "userprog/gdt.h"
+#include "userprog/pagedir.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"   /* is_user_vaddr, PHYS_BASE */
+#include "threads/palloc.h"
+#include "threads/pte.h"     /* pg_round_down */
+#include "threads/synch.h"
+#include "userprog/process.h" /* thread_current / process_exit if you have it */
 
 /* Number of page faults processed. */
 static long long page_fault_cnt;
 
 static void kill (struct intr_frame *);
+/* page_fault now handles user faults by terminating the user process
+   with exit(-1) instead of panicking the kernel. */
 static void page_fault (struct intr_frame *);
 
 /* Registers handlers for interrupts that can be caused by user
-   programs.
-
-   In a real Unix-like OS, most of these interrupts would be
-   passed along to the user process in the form of signals, as
-   described in [SV-386] 3-24 and 3-25, but we don't implement
-   signals.  Instead, we'll make them simply kill the user
-   process.
-
-   Page faults are an exception.  Here they are treated the same
-   way as other exceptions, but this will need to change to
-   implement virtual memory.
-
-   Refer to [IA32-v3a] section 5.15 "Exception and Interrupt
-   Reference" for a description of each of these exceptions. */
+   programs. */
 void
 exception_init (void) 
 {
-  /* These exceptions can be raised explicitly by a user program,
-     e.g. via the INT, INT3, INTO, and BOUND instructions.  Thus,
-     we set DPL==3, meaning that user programs are allowed to
-     invoke them via these instructions. */
   intr_register_int (3, 3, INTR_ON, kill, "#BP Breakpoint Exception");
   intr_register_int (4, 3, INTR_ON, kill, "#OF Overflow Exception");
   intr_register_int (5, 3, INTR_ON, kill,
                      "#BR BOUND Range Exceeded Exception");
 
-  /* These exceptions have DPL==0, preventing user processes from
-     invoking them via the INT instruction.  They can still be
-     caused indirectly, e.g. #DE can be caused by dividing by
-     0.  */
   intr_register_int (0, 0, INTR_ON, kill, "#DE Divide Error");
   intr_register_int (1, 0, INTR_ON, kill, "#DB Debug Exception");
   intr_register_int (6, 0, INTR_ON, kill, "#UD Invalid Opcode Exception");
-  intr_register_int (7, 0, INTR_ON, kill,
-                     "#NM Device Not Available Exception");
+  intr_register_int (7, 0, INTR_ON, kill, "#NM Device Not Available Exception");
   intr_register_int (11, 0, INTR_ON, kill, "#NP Segment Not Present");
   intr_register_int (12, 0, INTR_ON, kill, "#SS Stack Fault Exception");
   intr_register_int (13, 0, INTR_ON, kill, "#GP General Protection Exception");
   intr_register_int (16, 0, INTR_ON, kill, "#MF x87 FPU Floating-Point Error");
-  intr_register_int (19, 0, INTR_ON, kill,
-                     "#XF SIMD Floating-Point Exception");
+  intr_register_int (19, 0, INTR_ON, kill, "#XF SIMD Floating-Point Exception");
 
-  /* Most exceptions can be handled with interrupts turned on.
-     We need to disable interrupts for page faults because the
-     fault address is stored in CR2 and needs to be preserved. */
+  /* Page faults: we must disable interrupts while reading CR2. */
   intr_register_int (14, 0, INTR_OFF, page_fault, "#PF Page-Fault Exception");
 }
 
@@ -71,54 +56,65 @@ exception_print_stats (void)
 static void
 kill (struct intr_frame *f) 
 {
-  /* This interrupt is one (probably) caused by a user process.
-     For example, the process might have tried to access unmapped
-     virtual memory (a page fault).  For now, we simply kill the
-     user process.  Later, we'll want to handle page faults in
-     the kernel.  Real Unix-like operating systems pass most
-     exceptions back to the process via signals, but we don't
-     implement them. */
-     
-  /* The interrupt frame's code segment value tells us where the
-     exception originated. */
+  /* Determine where the exception originated (user or kernel). */
   switch (f->cs)
     {
     case SEL_UCSEG:
-      /* User's code segment, so it's a user exception, as we
-         expected.  Kill the user process.  */
+      /* User code caused the exception; kill the user process. */
       printf ("%s: dying due to interrupt %#04x (%s).\n",
               thread_name (), f->vec_no, intr_name (f->vec_no));
       intr_dump_frame (f);
       thread_exit (); 
+      break;
 
     case SEL_KCSEG:
-      /* Kernel's code segment, which indicates a kernel bug.
-         Kernel code shouldn't throw exceptions.  (Page faults
-         may cause kernel exceptions--but they shouldn't arrive
-         here.)  Panic the kernel to make the point.  */
+      /* Kernel code: kernel bug. Panic. */
       intr_dump_frame (f);
       PANIC ("Kernel bug - unexpected interrupt in kernel"); 
-
+      break;
     default:
-      /* Some other code segment?  Shouldn't happen.  Panic the
-         kernel. */
+      /* Unknown code segment. */
       printf ("Interrupt %#04x (%s) in unknown segment %04x\n",
              f->vec_no, intr_name (f->vec_no), f->cs);
       thread_exit ();
+      break;
     }
 }
 
-/* Page fault handler.  This is a skeleton that must be filled in
-   to implement virtual memory.  Some solutions to project 2 may
-   also require modifying this code.
+/* Page fault handler.
+   For faults caused by user-mode accesses, we must *not* panic the kernel.
+   Instead, terminate the user process with exit(-1) (and print the exit
+   message). Kernel-mode faults remain a PANIC. */
 
-   At entry, the address that faulted is in CR2 (Control Register
-   2) and information about the fault, formatted as described in
-   the PF_* macros in exception.h, is in F's error_code member.  The
-   example code here shows how to parse that information.  You
-   can find more information about both of these in the
-   description of "Interrupt 14--Page Fault Exception (#PF)" in
-   [IA32-v3a] section 5.15 "Exception and Interrupt Reference". */
+static bool 
+try_grow_stack (void *fault_addr, struct intr_frame *f){
+  void *upage = pg_round_down (fault_addr);
+  void *kpage;
+
+  //Verificar pra não crescer acima de PHYS_BASE (Kernel).
+  if (!is_user_vaddr (fault_addr) || fault_addr >= PHYS_BASE)
+    return false;
+  
+  if (f != NULL){
+    void *esp = f ->esp;
+    // Se esp for inválido, não crescer
+    if (!is_user_vaddr (esp))
+      return false;
+    
+    if ((fault_addr < esp - 32) && (pg_round_down(esp) != upage))
+      return false;
+  }
+  // Pegar pag física para o usuário:
+  kpage = palloc_get_page(PAL_USER | PAL_ZERO);
+  if (kpage == NULL)
+    return false;
+
+  if (!install_page(upage, kpage, true)){
+    palloc_free_page (kpage);
+    return false;
+  }
+  return true;
+}
 static void
 page_fault (struct intr_frame *f) 
 {
@@ -127,30 +123,36 @@ page_fault (struct intr_frame *f)
   bool user;         /* True: access by user, false: access by kernel. */
   void *fault_addr;  /* Fault address. */
 
-  /* Obtain faulting address, the virtual address that was
-     accessed to cause the fault.  It may point to code or to
-     data.  It is not necessarily the address of the instruction
-     that caused the fault (that's f->eip).
-     See [IA32-v2a] "MOV--Move to/from Control Registers" and
-     [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception
-     (#PF)". */
+  /* Get faulting address from CR2. */
   asm ("movl %%cr2, %0" : "=r" (fault_addr));
 
-  /* Turn interrupts back on (they were only off so that we could
-     be assured of reading CR2 before it changed). */
+  /* Re-enable interrupts. */
   intr_enable ();
 
   /* Count page faults. */
   page_fault_cnt++;
 
-  /* Determine cause. */
+  /* Parse error_code for cause. */
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  /* To implement virtual memory, delete the rest of the function
-     body, and replace it with code that brings in the page to
-     which fault_addr refers. */
+  /* If fault originated in user context, terminate the user process
+     with the required exit output (<name>: exit(-1)\n). */
+  if (user)
+    {
+      // Se for página não presente, tentar acrescimo de pilha
+      if (not_present){
+        if (try_grow_stack (fault_addr, f))
+          return; 
+      }
+      /* Print required exit message and terminate thread. */
+      printf ("%s: exit(%d)\n", thread_name (), -1);
+      thread_exit ();
+      return;
+    }
+
+  /* Otherwise, kernel fault -> print diagnostic and panic. */
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
@@ -158,4 +160,3 @@ page_fault (struct intr_frame *f)
           user ? "user" : "kernel");
   kill (f);
 }
-
